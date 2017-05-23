c6df67cf31abb51c4d97632a5f4411fb0dc321a9
pivotal <sgrewal@pivotal.io>
2017-05-23 19:24:26 -0400

    no message

--- pcf-cfops-backup/pipeline-work/task.sh
+++ pcf-cfops-backup/pipeline-work/task.sh
@@ -14,39 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-function main() {
-  local cwd
-  cwd="${1}"
 
-  chmod +x tool-om/om-linux
-  local om="tool-om/om-linux"
-
-  printf "Waiting for %s to come up" "$OPSMAN_URI"
-  until $(curl --output /dev/null --silent --head --fail -k ${OPSMAN_URI}); do
-    printf '.'
-    sleep 5
-  done
-  printf '\n'
-
-  $om --target "${OPSMAN_URI}" \
-      --skip-ssl-validation \
-      import-installation \
-      --installation "${cwd}/opsmgr-settings/${OPSMAN_SETTINGS_FILENAME}" \
-      --decryption-passphrase "${OPSMAN_PASSPHRASE}"
- }
 
  sudo apt-get update
  sudo apt-get install sshpass
 
  echo "Updating routes on opsman"
 
- echo "Setting  ${ROUTE1}"
- sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ${ROUTE1}
- echo "Setting  ${ROUTE2}"
- sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ${ROUTE2}
- echo "Setting  ${ROUTE3}"
- sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ${ROUTE3}
- echo "Setting  ${ROUTE4}"
- sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ${ROUTE4}
+
+ sshpass -e ssh -o StrictHostKeyChecking=no ubuntu@${OPSMAN_IP} ls
 
  main "${PWD}"
